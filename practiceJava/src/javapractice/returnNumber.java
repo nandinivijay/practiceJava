577f7b7783db7244bee93b1258ed8dea1f631b8e
sabuhiiqbal <iqbal.sabuhi@gmail.com>
2020-12-07 13:34:29 -0500

    firstcommit

---
+++ practiceJava/src/javapractice/returnNumber.java
@@ -0,0 +1,22 @@
+
+package javapractice;
+
+
+
+public class returnNumber {
+	public static void main(String[] args) {
+		
+		int[] arr = {7,4,5,8,8,9};
+		
+		
+		int i, k = 0;
+		for (i = 0; i < arr.length; i++)
+		    k = 10 * k + arr[i];
+		
+		System.out.println(k);
+		}
+		
+		
+		
+}
+
